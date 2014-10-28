941d38afa9b3cb65acbc50aa3031487ab5799c48
Andreas Lengqvist <al223bn@student.lnu.se>
2014-10-28 18:40:42 +0100

    update

---
+++ BlackJack/controller/PlayGame.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace BlackJack.controller
+{
+    class PlayGame : IObserver<model.Card>
+    {
+
+        private model.Game a_game;
+        private view.IView a_view;
+
+
+
+        public PlayGame(model.Game game, view.IView view)
+        {
+            a_game = game;
+            a_view = view;
+        }
+
+
+        public bool Play()
+        {
+
+
+            a_game.GetPlayer().Subscribe(this);
+            a_game.GetDealer().Subscribe(this);
+
+            RenderOut();
+
+
+            if (a_game.IsGameOver())
+            {
+                a_view.DisplayGameOver(a_game.IsDealerWinner());
+            }
+
+            int input = a_view.GetInput();
+
+            if (a_view.ActionNewGame(input))
+            {
+                a_game.NewGame();
+            }
+            if (a_view.ActionStand(input))
+            {
+                a_game.Stand();
+            }
+            if (a_view.ActionHit(input))
+            {
+                a_game.Hit();
+            }
+
+            return a_view.ActionQuit(input) == false;
+        }
+
+        public void OnNext(model.Card card)
+        {
+            Thread.Sleep(500);
+            RenderOut();
+        }
+
+        public void RenderOut()
+        {
+            a_view.DisplayWelcomeMessage();
+
+            a_view.DisplayDealerHand(a_game.GetDealerHand(), a_game.GetDealerScore());
+            a_view.DisplayPlayerHand(a_game.GetPlayerHand(), a_game.GetPlayerScore());
+        }
+
+    }
+}
---
+++ BlackJack/view/IView.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BlackJack.view
+{
+    interface IView
+    {
+        void DisplayWelcomeMessage();
+        int GetInput();
+        bool ActionNewGame(int input);
+        bool ActionStand(int input);
+        bool ActionHit(int input);
+        bool ActionQuit(int input);
+        void DisplayCard(model.Card a_card);
+        void DisplayPlayerHand(IEnumerable<model.Card> a_hand, int a_score);
+        void DisplayDealerHand(IEnumerable<model.Card> a_hand, int a_score);
+        void DisplayGameOver(bool a_dealerIsWinner);
+    }
+}
